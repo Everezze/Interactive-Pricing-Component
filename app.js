a71518e10ebb8f5afccea17bb929d21d7c0f8d8c
Everezze <83906211+Everezze@users.noreply.github.com>
2021-08-30 17:52:10 +0200

    slider thumb and track styled, toggle button dynamic with JS, calculations left

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const discount= document.getElementById('discount');
+const toggleButton= document.getElementById('toggleButton');
+const toggleButtonInnerCircle=document.getElementById('toggleCircle');
 
 
 window.addEventListener('resize', function(){
@@ -8,6 +10,24 @@ window.addEventListener('resize', function(){
     }
     else{discount.innerHTML='-25%';}
 
+    //There might be unnecessary use of this event as not everyone will resize its window.
+    //Just putting the condition might be a better solution. 
+});
+
+window.addEventListener('load', function(){
+
+    if(window.innerWidth >= 782){
+        discount.innerHTML = "-25% discount";
+    }
+    else{discount.innerHTML='-25%';}
 })
 
+toggleButton.addEventListener('click', function(){
+    toggleButton.classList.toggle('active');
 
+    if(toggleButton.classList.contains('active')){
+        console.log("entered condition")
+        toggleButtonInnerCircle.style.marginLeft=`calc(100% - 15px)`;
+    }
+    else{toggleButtonInnerCircle.style.marginLeft='0';}
+});
