b6154a7ad0a0b56e5ed297a2c9c8ef33240846f0
Everezze <83906211+Everezze@users.noreply.github.com>
2021-08-31 17:30:19 +0200

    changing price interactively with slider works, the discount is also correctly applied

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const toggleContainer= document.getElementById('toggleContainer');
 const yearlyBilling= document.getElementById('yearlyBilling');
 const slider= document.getElementById('sliderRange');
 const price= document.getElementById('price');
+const pageviews= document.getElementById('cardTitle');
 
+const prices= [8,12,16,24,36];
 
 window.addEventListener('resize', function(){
 
@@ -42,6 +44,30 @@ toggleButton.addEventListener('click', function(){
         toggleContainer.style.padding='0 1rem';
     }
 
+
+    switch(parseFloat(slider.value)){
+        case 8:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[0]*12*(1-25/100)}`:price.innerHTML=`$${prices[0]}`;
+            break;
+        
+        case 15:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[1]*12*(1-25/100)}`:price.innerHTML=`$${prices[1]}`;
+            break;
+        
+        case 22:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[2]*12*(1-25/100)}`:price.innerHTML=`$${prices[2]}`;
+            break;
+        
+        case 29:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[3]*12*(1-25/100)}`:price.innerHTML=`$${prices[3]}`;
+            break;
+        
+        case 36:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[4]*12*(1-25/100)}`:price.innerHTML=`$${prices[4]}`;
+            break;
+    }
+
+
     if(toggleButton.classList.contains('active') && window.innerWidth >=782 || window.innerWidth<782){
         console.log("aa");
         yearlyBilling.style.marginRight="0";
@@ -52,26 +78,59 @@ toggleButton.addEventListener('click', function(){
 });
 
 slider.addEventListener('change', function(){
-    let step=slider.getAttribute('step');
-    console.log('zerzef');
-    if(parseFloat(slider.value)>=16 && slider.value<24){
-        step = 8;
-        console.log(`ici step fait ${step}`);
-        slider.setAttribute('step',step);
-    }
-    else if(parseFloat(slider.value)>=24 && slider.value<36){
-        step= 12;
-        console.log(`ici step fait ${step}`);
-        slider.setAttribute('step',step);
-    }   
-    else{
-        step=4;
-        console.log(`ici step fait ${step}`);
-        slider.setAttribute('step',step);
-    }
+    // let step=slider.getAttribute('step');
+    // console.log('zerzef');
+    // if(parseFloat(slider.value)>=16 && slider.value<24){
+    //     step = 8;
+    //     console.log(`ici step fait ${step}`);
+    //     slider.setAttribute('step',step);
+    // }
+    // else if(parseFloat(slider.value)>=24 && slider.value<36){
+    //     step= 12;
+    //     console.log(`ici step fait ${step}`);
+    //     slider.setAttribute('step',step);
+    // }   
+    // else{
+    //     step=4;
+    //     console.log(`ici step fait ${step}`);
+    //     slider.setAttribute('step',step);
+    // }
 
     // switch(slider.value){
     //     case >=16<24:
     // }
-    price.innerHTML=slider.value;
+
+
+    switch(parseFloat(slider.value)){
+        case 8:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[0]*12*(1-25/100)}`:price.innerHTML=`$${prices[0]}`;
+            pageviews.innerHTML="10k pageviews";
+            // price.innerHTML =`$${prices[0]}`;
+            break;
+        
+        case 15:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[1]*12*(1-25/100)}`:price.innerHTML=`$${prices[1]}`;
+            pageviews.innerHTML="50k pageviews";
+            // price.innerHTML=`$${prices[1]}`;
+            break;
+        
+        case 22:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[2]*12*(1-25/100)}`:price.innerHTML=`$${prices[2]}`;
+            pageviews.innerHTML="100k pageviews";
+            // price.innerHTML='$16.00';
+            break;
+        
+        case 29:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[3]*12*(1-25/100)}`:price.innerHTML=`$${prices[3]}`;
+            pageviews.innerHTML="500k pageviews";
+            // price.innerHTML='$24.00';
+            break;
+        
+        case 36:
+            toggleButton.classList.contains('active')? price.innerHTML=`$${prices[4]*12*(1-25/100)}`:price.innerHTML=`$${prices[4]}`;
+            pageviews.innerHTML="1m pageviews";
+            // price.innerHTML='$36.00';
+            break;
+    }
+
 });
